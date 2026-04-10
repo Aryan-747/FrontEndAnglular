5d5ca906e779cf3f9806a420fcb898cf41413f3a
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2026-04-10 14:36:14 +0530

    jagged

---
+++ Day1JS/func5.js
@@ -0,0 +1,6 @@
+const player = {pid:101}
+const{pid,pname = "Sai Kishore"} = player;
+
+console.log("Player Information");
+console.log(`Player ID: ${pid}`);
+console.log(`Player Name: ${pname}`);
---
+++ Day1JS/func6.js
@@ -0,0 +1,16 @@
+const order = {
+    orderId: 5001,
+    customer: {
+        name:"Sam",
+        email: "sam32@gmail.com",
+
+    },
+    items: [
+        {product: "Laptop", price:5000},
+        {product: "Phone", price:4500}
+    ],
+    shipping : {
+        city: "Delhi",
+        deliveryDays: 2,
+    }
+};
